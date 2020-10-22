8da283f21c4b87dc7aeb7870a195b29863bdba32
sergio <serg.ribmat@gmail.com>
2020-10-22 22:16:11 +0200

    add multiple selection intent

--- app/src/main/java/com/example/testingenvironment/MainActivity.kt
+++ app/src/main/java/com/example/testingenvironment/MainActivity.kt
@@ -1,50 +1,55 @@
 package com.example.testingenvironment
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
 
-const val REQUEST_IMAGE_GET = 1
+const val REQUEST_IMAGE_GET = 101
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setGetImageButtonListener()
+    }
+
+    fun setGetImageButtonListener() {
         this.findViewById<Button>(R.id.get_image_button).setOnClickListener {
             selectImage()
         }
     }
 
-    fun getImageButtonAction() {
-
-    }
-
-    fun showToast(text: String){
+    fun showToast(text: String) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
-    fun createIntent(action: String, typeString: String): Intent{
+    fun createIntent(action: String, typeString: String): Intent {
         return Intent(action).apply {
             type = typeString
         }
     }
 
 
-
     fun selectImage() {
         intent = createIntent(Intent.ACTION_GET_CONTENT, "image/*")
+            .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         //if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_GET)
+        startActivityForResult(intent, REQUEST_IMAGE_GET)
         //}
     }
 
@@ -57,14 +62,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun printImageWithGlide(context: Context, imgUri: Uri?, imgView: ImageView){
+    fun printImageWithGlide(context: Context, imgUri: Uri?, imgView: ImageView) {
         Glide.with(context)
             .load(imgUri)
             .apply(
                 RequestOptions()
-                .placeholder(R.drawable.ic_launcher_background)
-                .error(R.drawable.ic_launcher_background))
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+            )
             .into(imgView)
     }
 
 }
+
+
